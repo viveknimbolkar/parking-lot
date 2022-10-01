a03a56cbe0ff46c9e4d59b3df9b184f70560c043
viveknimbolkar <viveknimbolkar50@gmail.com>
2022-10-01 15:28:08 +0530

    add user interface to get and show information to the user

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <center>
+          <h1 className="heading">Parking System</h1>
+        </center>
       </header>
-    </div>
+      <section id="user-section">
+        <div className="vehicalForm">
+          <label>Vehicle Number</label>
+          <br></br>
+          <input type="text"></input>
+          <br></br>
+          <br></br>
+          <label>Driver Name</label>
+          <br></br>
+          <input type="text"></input>
+          <br></br> <br></br>
+          <input type="submit" className="saveBtn"></input>
+        </div>
+
+        <div className="vehicle-section">
+          <h4>Total Vehicles</h4>
+          <p>456</p>
+        </div>
+      </section>
+
+      <section className="vehicle-list">
+        <table>
+          <thead>
+            <td>Vehicle Number</td>
+            <td>Driver Name</td>
+            <td>Check-in</td>
+            <td>Check-out</td>
+          </thead>
+          <tbody>
+            <tr>
+              <td>MH01AE8017</td>
+              <td>John Doe</td>
+              <td>4:55</td>
+              <td>16:34</td>
+            </tr>
+            <tr>
+              <td>MH01AE8017</td>
+              <td>John Doe</td>
+              <td>4:55</td>
+              <td>16:34</td>
+            </tr>
+            <tr>
+              <td>MH01AE8017</td>
+              <td>John Doe</td>
+              <td>4:55</td>
+              <td>16:34</td>
+            </tr>
+            <tr>
+              <td>MH01AE8017</td>
+              <td>John Doe</td>
+              <td>4:55</td>
+              <td>16:34</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+    </>
   );
 }
 
